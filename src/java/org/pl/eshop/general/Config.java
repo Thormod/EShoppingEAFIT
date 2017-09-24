b1309961518ee65b0b04d6e9305194d12998037f
Thormod <jszapata94@gmail.com>
2017-09-23 21:15:50 -0500

    First commit

---
+++ src/java/org/pl/eshop/general/Config.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Thormod
+ */
+package org.pl.eshop.general;
+
+
+public class Config {
+    public static final String SERVIDOR = "127.0.0.1";
+    public static final int PUERTO = 3306;
+    public static final String BD = "tienda";
+    public static final String NOMBRE_USUARIO = "root";
+    public static final String CONTRASENA_USUARIO = "";
+}
